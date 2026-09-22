10eecd1d016e6d0f0ccb250d86afd4d3f1c7b7b8
Peter Enbom <peter@enbom.dev>
2026-09-22 11:28:43 +0200

    Add Git pull to deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+set -e
 
-cp /home/db/config-project/index.html /srv/www/htdocs/index.html
+git -C /home/db/config-project pull --ff-only origin main
+sudo cp /home/db/config-project/index.html /srv/www/htdocs/index.html
