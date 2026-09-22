9259d17dd1f916899c5db4f0b34148b434186f11
Peter Enbom <peter@enbom.dev>
2026-09-22 12:46:18 +0200

    Update deployment script for automated cron deployment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-git -C /home/db/config-project pull --ff-only origin main
-sudo cp /home/db/config-project/index.html /srv/www/htdocs/index.html
+/usr/bin/git -C /home/db/config-project pull --ff-only origin main
+/usr/bin/sudo /usr/bin/cp /home/db/config-project/index.html /srv/www/htdocs/index.html
